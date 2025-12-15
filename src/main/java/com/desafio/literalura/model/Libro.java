f75434b3c1dff57ee079b471ec18f611b2433ab4
EduarMendozaL <edmenlla@gmail.com>
2025-12-15 17:35:50 -0300

    Creando la clase Libro con sus getters, setters y toString

---
+++ src/main/java/com/desafio/literalura/model/Libro.java
@@ -0,0 +1,50 @@
+package com.desafio.literalura.model;
+
+public class Libro {
+    private String titulo;
+    private String autor;
+    private String idioma;
+    private Double numeroDeDescargas;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    public Double getNumeroDeDescargas() {
+        return numeroDeDescargas;
+    }
+
+    public void setNumeroDeDescargas(Double numeroDeDescargas) {
+        this.numeroDeDescargas = numeroDeDescargas;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", idioma='" + idioma + '\'' +
+                ", numeroDeDescargas=" + numeroDeDescargas +
+                '}';
+    }
+}
