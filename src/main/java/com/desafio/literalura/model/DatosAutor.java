8ca9dc02c6deb533b4840d2a0f8893236488c1f9
EduarMendozaL <edmenlla@gmail.com>
2025-12-16 16:04:44 -0300

    Cambiando tipo de datos

--- src/main/java/com/desafio/literalura/model/DatosAutor.java
+++ src/main/java/com/desafio/literalura/model/DatosAutor.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DatosAutor(
         @JsonAlias("name") String nombre,
-        @JsonAlias("birth_year") String fechaNacimiento,
-        @JsonAlias("death_year") String fechaDefuncion
+        @JsonAlias("birth_year") Integer anioNacimiento,
+        @JsonAlias("death_year") Integer anioFallecimiento
 ) {
 }
