92bd87fc187d539048e14515f0e660adda22c000
EduarMendozaL <edmenlla@gmail.com>
2025-12-16 16:03:31 -0300

    Creando la clase Autor

---
+++ src/main/java/com/desafio/literalura/model/Autor.java
@@ -0,0 +1,54 @@
+package com.desafio.literalura.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Autores")
+public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private Integer anioNacimiento;
+    private Integer anioFallecimiento;
+
+    public Autor() {}
+
+    public Autor(DatosAutor datosAutor) {
+        this.nombre = datosAutor.nombre();
+        this.anioNacimiento = datosAutor.anioNacimiento();
+        this.anioFallecimiento = datosAutor.anioFallecimiento();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getAnioNacimiento() {
+        return anioNacimiento;
+    }
+
+    public void setAnioNacimiento(Integer anioNacimiento) {
+        this.anioNacimiento = anioNacimiento;
+    }
+
+    public Integer getAnioFallecimiento() {
+        return anioFallecimiento;
+    }
+
+    public void setAnioFallecimiento(Integer anioFallecimiento) {
+        this.anioFallecimiento = anioFallecimiento;
+    }
+}
