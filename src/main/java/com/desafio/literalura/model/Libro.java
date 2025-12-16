8e099e9b738f4b65e1b6a626e8d0635328c1ddce
EduarMendozaL <edmenlla@gmail.com>
2025-12-16 16:03:04 -0300

    Creando la clase Libro

---
+++ src/main/java/com/desafio/literalura/model/Libro.java
@@ -0,0 +1,71 @@
+package com.desafio.literalura.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Libros")
+public class Libro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    @ManyToMany
+    @JoinTable(
+            name = "Libros_autores",
+            joinColumns = @JoinColumn(name = "libro_id"),
+            inverseJoinColumns = @JoinColumn(name = "autor_id")
+    )
+    private List<Autor> autores;
+    private String idioma;
+    private Double numeroDeDescargas;
+
+    public Libro() {}
+
+    public Libro(DatosLibro datosLibro) {
+        this.titulo = datosLibro.titulo();
+        this.idioma = datosLibro.idiomas().get(0);
+        this.numeroDeDescargas = datosLibro.numeroDeDescargas();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public List<Autor> getAutores() {
+        return autores;
+    }
+
+    public void setAutores(List<Autor> autores) {
+        this.autores = autores;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    public Double getNumeroDeDescargas() {
+        return numeroDeDescargas;
+    }
+
+    public void setNumeroDeDescargas(Double numeroDeDescargas) {
+        this.numeroDeDescargas = numeroDeDescargas;
+    }
+}
