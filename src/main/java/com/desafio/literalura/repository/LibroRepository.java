406269681fdfe2569b8010949e82eb6091391bf1
EduarMendozaL <edmenlla@gmail.com>
2025-12-19 12:54:58 -0300

    Creando la consulta para buscar libros por idioma

--- src/main/java/com/desafio/literalura/repository/LibroRepository.java
+++ src/main/java/com/desafio/literalura/repository/LibroRepository.java
@@ -3,6 +3,10 @@ package com.desafio.literalura.repository;
 import com.desafio.literalura.model.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LibroRepository extends JpaRepository<Libro, Long> {
     boolean existsByTitulo(String titulo);
+
+    List<Libro> findByIdiomaIgnoreCase(String idioma);
 }
