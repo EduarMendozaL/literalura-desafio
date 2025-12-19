76159d9bdb59e8f8335b88348f0db89418064128
EduarMendozaL <edmenlla@gmail.com>
2025-12-19 12:48:49 -0300

    Creando la consulta para mostrar autores por año

--- src/main/java/com/desafio/literalura/repository/AutorRepository.java
+++ src/main/java/com/desafio/literalura/repository/AutorRepository.java
@@ -2,9 +2,15 @@ package com.desafio.literalura.repository;
 
 import com.desafio.literalura.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AutorRepository extends JpaRepository<Autor, Long> {
     Optional<Autor> findByNombre(String nombre);
+
+    @Query("SELECT a FROM Autor a WHERE a.anioNacimiento <= :anio AND (a.anioFallecimiento IS NULL OR a.anioFallecimiento > :anio)")
+    List<Autor> autoresVivosEnAnio(@Param("anio") Integer anio);
 }
